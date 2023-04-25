2b5d5f432c35f6619dca5711989f67da35af6cd4
VictorMUribe <111189859+VictorMUribe@users.noreply.github.com>
2023-04-25 18:49:33 -0500

    Updating all files

--- Loading_Data.R
+++ Loading_Data.R
@@ -1,11 +1,10 @@
 library(tidyverse)
 library(tidymodels)
-library(data.table)
-library(timetk)
-library(modeltime)
 
-members <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-22/members.csv')
-expeditions <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-22/expeditions.csv')
+
+
+members <- data.table::fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-22/members.csv')
+expeditions <- data.table::fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-22/expeditions.csv')
 #peaks <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-22/peaks.csv')
 
 
@@ -34,31 +33,62 @@ expeditions <- fread('https://raw.githubusercontent.com/rfordatascience/tidytues
 
 
 
+
+#everest <- members %>% # should have an age
+#  filter(age != 'NA' & peak_name == "Everest" ) %>% 
+#  select(-c(peak_id,peak_name,expedition_id, member_id, death_height_metres,
+#            injury_height_metres, highpoint_metres, death_cause, injury_type)) %>%
+#  mutate(
+#    #death_height_metres = ifelse(death_height_metres == NA, 0, death_height_metres), # issue wiith levels for some reason
+#    #injury_height_metres = ifelse(injury_height_metres == NA, 0, injury_height_metres), # gives leveling issue 
+#    #highpoint_metres = ifelse(highpoint_metres == 'NA', 0, highpoint_metres), # thows off the p values
+#    season = factor(season),
+#    sex = factor(sex),
+#    citizenship = factor(citizenship), # not significant after testing
+#    expedition_role = factor(expedition_role), # not significant after testing
+#    hired = factor(hired), 
+#    success = factor(success), # value being predicted
+#    solo = factor(solo),
+#    oxygen_used = factor(oxygen_used),
+#    died = factor(died),
+    #death_cause = factor(death_cause), # issue
+#    injured = factor(injured)#,
+    #injury_type = factor(injury_type) # issue with levels
+#  ) 
+
+
+
+# post processing 
 everest <- members %>% # should have an age
-  filter(age != 'NA' & peak_name == "Everest") %>% 
-  select(-c(peak_id,peak_name,expedition_id,member_id, death_height_metres,
-            injury_height_metres, highpoint_metres, death_cause,injury_type)) %>%
+  filter(age != 'NA' & peak_name == "Everest" & expedition_role == "Climber") %>% 
+  select(-c(peak_id,peak_name,expedition_id, member_id, expedition_role, death_height_metres,
+            injury_height_metres, highpoint_metres, death_cause, injury_type, hired, solo)) %>%
   mutate(
     #death_height_metres = ifelse(death_height_metres == NA, 0, death_height_metres), # issue wiith levels for some reason
     #injury_height_metres = ifelse(injury_height_metres == NA, 0, injury_height_metres), # gives leveling issue 
     #highpoint_metres = ifelse(highpoint_metres == 'NA', 0, highpoint_metres), # thows off the p values
     season = factor(season),
     sex = factor(sex),
-    #citizenship = factor(citizenship), # not significant after testing
+    citizenship = factor(citizenship), # not significant after testing
     #expedition_role = factor(expedition_role), # not significant after testing
-    hired = factor(hired), 
+    #hired = factor(hired), 
     success = factor(success), # value being predicted
-    solo = factor(solo),
+    #solo = factor(solo),
     oxygen_used = factor(oxygen_used),
     died = factor(died),
     #death_cause = factor(death_cause), # issue
     injured = factor(injured)#,
     #injury_type = factor(injury_type) # issue with levels
-  )
+  ) 
   
 
-#everest %>% names()
+prop_na <- function(col){
+  sum(is.na(col))/length(col)  
+}
 
+data.frame(prop = sapply(data.frame(everest), prop_na) ) %>% 
+  select(prop) %>% 
+  filter(prop > 0)
 
 
 
@@ -70,7 +100,6 @@ everest_train <- training(everest_split)
 everest_test <- testing(everest_split)
 
 everest_fold <- vfold_cv(everest_train, strata = success)
-
 
 
 
